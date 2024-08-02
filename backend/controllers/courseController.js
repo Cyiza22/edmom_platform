49e539c6877e4651c8b17194573633c661258998
Cyiza22 <127378914+Cyiza22@users.noreply.github.com>
2024-08-02 20:44:15 +0200

    add

---
+++ backend/controllers/courseController.js
@@ -0,0 +1,29 @@
+const Course = require('../models/Course');
+
+exports.addCourse = async (req, res) => {
+    const { title, description, level } = req.body;
+
+    try {
+        const newCourse = new Course({
+            title,
+            description,
+            level
+        });
+
+        const course = await newCourse.save();
+        res.json(course);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+exports.getCourses = async (req, res) => {
+    try {
+        const courses = await Course.find();
+        res.json(courses);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
